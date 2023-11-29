07da5402b8d616aea9dd8e8da44039d1b8e0289b
DaGiBi <najibaseri@gmail.com>
2023-11-29 21:51:35 +0800

    add mongo db

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 
+import mongoose from 'mongoose';
+
 const app = express();
 
 app.use(cors({
@@ -20,3 +22,9 @@ const server = http.createServer(app);
 server.listen(8080, () => {
     console.log('server running on http://localhost:8080/');
 })
+
+const MONGO_URL = 'mongodb+srv://gibisteam15:ymmtLEUHiRPsmmgu@cluster0.mxw0bug.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
