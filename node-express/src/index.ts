f8472a204f6d857a1382902900dc7e826c532ee3
DaGiBi <najibaseri@gmail.com>
2023-11-30 00:57:30 +0800

    REFACTOR

--- node-express/src/index.ts
+++ node-express/src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import * as dotenv from 'dotenv'
 
 import mongoose from 'mongoose';
 
@@ -21,10 +22,11 @@ const server = http.createServer(app);
 
 server.listen(8080, () => {
     console.log('server running on http://localhost:8080/');
-})
-
-const MONGO_URL = 'mongodb+srv://gibisteam15:ymmtLEUHiRPsmmgu@cluster0.mxw0bug.mongodb.net/?retryWrites=true&w=majority'
-
+});
+const dotenv_1 = require("dotenv");
+dotenv_1.config();
+const port = process.env.PORT;
+const MONGO_URL = process.env.DATABASE_URL
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
